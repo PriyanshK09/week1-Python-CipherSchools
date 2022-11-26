13312dcc37dac1da4ae3c4b091a4176ef2813e49
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-11-27 02:01:56 +0530

    Day 3 Projects added to the repository

---
+++ Day-3/project08.py
@@ -0,0 +1,10 @@
+#Keyword in Python
+name = "Priyansh"
+
+#in keyword is used to check if a value is present in a sequence or not
+#if with in keyword
+if "P" in name:
+    print("Yes, P is present in name")
+else:
+    print("No, P is not present in name")
+    
