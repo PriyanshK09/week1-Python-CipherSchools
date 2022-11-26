219137e856ace24d065ad0dcb4b9bd1353c9f53e
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-11-26 23:27:22 +0530

    Day 2 Projects added to the repository

--- Day-2/project02.py
+++ Day-2/project02.py
@@ -10,3 +10,12 @@ print(name.lstrip()+dots)
 rstrip_name=name.rstrip()
 print(f"{dots}Removing Right Side Spaces{dots}")
 print(name.rstrip()+dots)
+
+#Removing Both Side Spaces
+strip_name=name.strip()
+print(f"{dots}Removing Both Side Spaces{dots}")
+print(name.strip()+dots)
+
+#For a string with Spaces between words
+print(name.replace(" ","")+dots)
+
