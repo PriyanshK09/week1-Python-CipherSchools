6db27fe588d25cb43384f4de06ed0d150a7ced35
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-11-26 23:09:55 +0530

    Day 1 Projects added to the repository

---
+++ Day-1/project09.py
@@ -0,0 +1,5 @@
+#Step Arguments in Python
+# Syntax of Step Arguments in Python : [start argument : stop argument : step argument]
+
+lang = "Python"
+print(lang[0:6:2]) #Pto
---
+++ Day-1/project10.py
@@ -0,0 +1,4 @@
+#Ask user name and print the name in reverse order using String Formatting.
+name = input("Enter your name: ")
+reverse=name[-1::-1]
+print(f"Your name in reverse order is {reverse}")
