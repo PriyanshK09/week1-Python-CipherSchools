21287650baece865c8ef89b7afba6d40ca2921a5
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-11-27 00:54:37 +0530

    Day 2 Projects added to the repository

---
+++ Day-3/project03.py
@@ -0,0 +1,8 @@
+#Else Statement Python
+age = int(input("Enter your age: "))
+if age > 18:
+    print("You are eligible to vote")
+else :
+    print("You are not eligible to vote")
+    
+#Else statment is used after IF statement in python to execute a block of code when the condition is false.
