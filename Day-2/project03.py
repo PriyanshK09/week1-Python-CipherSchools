a7201a42e3c9078381a3b9589df91aef25ab60aa
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-11-26 23:36:56 +0530

    Day 2 Projects added to the repository

---
+++ Day-2/project03.py
@@ -0,0 +1,10 @@
+#Find and Replace in Python
+
+#replace() method is used to replace a substring in a string with another substring.
+#find() method is used to find a substring in a string.
+
+string="She is beutiful and she is good dancer"
+print(string.replace(" ", "_")) #Replacing Space
+print(string.replace("is", "was")) #Replacing a Word/String from Senetence
+
+print(string.find("is")) #Finding a Word/String from Senetence
