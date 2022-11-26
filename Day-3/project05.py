56ab7f9e6e4b06d708f1678ebbf71e395ae40d2d
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-11-27 01:31:42 +0530

    Day 3 Projects added to the repository

---
+++ Day-3/project05.py
@@ -0,0 +1,29 @@
+# And, Or Operator in Python
+# Check two conditions at the same time
+# using AND operator in python, OR operator in python
+
+# AND operator in python
+
+name = "ABC"
+age=19
+
+if name == "ABC" and age == 19:
+    print("Condition True")
+    
+else:
+    print("Condition False")
+    
+    
+# OR operator in python
+
+name = "ABC"
+age=19
+
+if name == "ABC" or age == 19:
+    print("Condition True")
+    
+else:
+    print("Condition False")
+    
+#AND operator is used to check two conditions at the same time and both conditions must be true
+#OR operator is used to check two conditions at the same time and any one condition must be true
