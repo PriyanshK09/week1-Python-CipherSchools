16ef38d59818a69a18cd23bda04222b4ba43970b
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-11-27 01:21:23 +0530

    Day 3 Projects added to the repository

--- Day-3/project01.py
+++ Day-3/project01.py
@@ -7,3 +7,4 @@ age = input("Enter your age: ")
 age = int(age)
 if age >= 18:
     print("You are eligible to Play the Game")
+
--- Day-3/project03.py
+++ Day-3/project03.py
@@ -6,3 +6,4 @@ else :
     print("You are not eligible to vote")
     
 #Else statment is used after IF statement in python to execute a block of code when the condition is false.
+
